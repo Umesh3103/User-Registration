72ec377fe230f83078953daf4dc8e330e731ed06
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-29 16:56:00 +0530

    [Umesh]Added: checking validity of password must contains one uppercase alphabet

--- src/com/learning/Registration.java
+++ src/com/learning/Registration.java
@@ -2,14 +2,14 @@ package com.learning;
 import java.util.regex.*;
 
 public class Registration {
-	private static final Pattern Password = Pattern.compile("[a-z A-Z 0-9]{8,}");
+	private static final Pattern Password = Pattern.compile("(?=.*[A-Z])([a-z A-Z 0-9]).{8,}");
     private static boolean checkName(String name) {
         return Password.matcher(name).matches();
     }
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration problem");
-		boolean flag =checkName("6648124abAc");
+		boolean flag =checkName("AA6648124ABabc");
 		if(flag){
 			System.out.println("Valid password");
 		}
