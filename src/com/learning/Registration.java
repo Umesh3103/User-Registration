72d9c5bff753c5b5cb665fac8e6c801872779f60
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-29 17:21:24 +0530

    [Umesh]Added: checking validity of password must contains atleast one special character

--- src/com/learning/Registration.java
+++ src/com/learning/Registration.java
@@ -2,14 +2,14 @@ package com.learning;
 import java.util.regex.*;
 
 public class Registration {
-	private static final Pattern Password = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])([a-z A-Z 0-9]).{6,}");
+	private static final Pattern Password = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])(?=.*[_+-.@#$])([a-z A-Z 0-9]).{5,}");
     private static boolean checkName(String name) {
         return Password.matcher(name).matches();
     }
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration problem");
-		boolean flag =checkName("ab1Aabca");
+		boolean flag =checkName("ab1@Aabca");
 		if(flag){
 			System.out.println("Valid password");
 		}
