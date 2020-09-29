a99c64c354bd4d0376291f5bddab972d742a753f
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-29 16:19:03 +0530

    [Umesh]Added: checking validity of email

--- src/com/learning/Registration.java
+++ src/com/learning/Registration.java
@@ -2,19 +2,19 @@ package com.learning;
 import java.util.regex.*;
 
 public class Registration {
-	private static final Pattern NAME_PATTERN = Pattern.compile("[A-Z]{1}[a-z]{2,}");
+	private static final Pattern Email = Pattern.compile("^[0-9 a-z A-Z]+([_+-.]{0,1}[0-9 a-z A-Z]+)[@][0-9 a-z A-Z]+[.][a-z A-Z]{2,4}([.][a-z A-Z]{2}){0,1}$");
     private static boolean checkName(String name) {
-        return NAME_PATTERN.matcher(name).matches();
+        return Email.matcher(name).matches();
     }
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration problem");
-		boolean flag =checkName("Sharma");
+		boolean flag =checkName("abc.xyz@gmail.co.in");
 		if(flag){
-			System.out.println("Valid Last name");
+			System.out.println("Valid email");
 		}
 		else{
-			System.out.println("Invalid Last name");
+			System.out.println("Invalid email");
 		}
 	}
 	
