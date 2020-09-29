64c40ee1e50f1cf514dbc2ff8590dbd4b5f67a63
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-29 17:12:50 +0530

    [Umesh]Added: checking validity of password must contains one digit

--- src/com/learning/Registration.java
+++ src/com/learning/Registration.java
@@ -2,14 +2,14 @@ package com.learning;
 import java.util.regex.*;
 
 public class Registration {
-	private static final Pattern Password = Pattern.compile("(?=.*[A-Z])([a-z A-Z 0-9]).{8,}");
+	private static final Pattern Password = Pattern.compile("(?=.*[A-Z])(?=.*[0-9])([a-z A-Z 0-9]).{6,}");
     private static boolean checkName(String name) {
         return Password.matcher(name).matches();
     }
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration problem");
-		boolean flag =checkName("AA6648124ABabc");
+		boolean flag =checkName("ab1Aabca");
 		if(flag){
 			System.out.println("Valid password");
 		}
